2b367b0e2cf3b93f857737debf6a95df0d9ea5c2
Rusta <rustauk@gmail.com>
2017-06-23 20:49:11 +0100

    some linting

--- src/component/App/index.js
+++ src/component/App/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class App extends Component {
   render() {
@@ -6,8 +6,8 @@ class App extends Component {
       <div className="App">
         <p> Hello World </p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
