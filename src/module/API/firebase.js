29a1f5dc6c7e64583cb614a1ec4ab425355dee99
Christopher Pitt <c.james.pitt@gmail.com>
2017-06-23 20:37:01 +0100

    add basic firebase connection details and bootstrapping

---
+++ src/module/API/firebase.js
@@ -0,0 +1,12 @@
+const config = {
+  apiKey: "AIzaSyCDUBQ1zY5mFekbXhYvgjyilBtP3uxmkMI",
+  authDomain: "the92club-a8d3a.firebaseapp.com",
+  databaseURL: "https://the92club-a8d3a.firebaseio.com",
+  projectId: "the92club-a8d3a",
+  storageBucket: "the92club-a8d3a.appspot.com",
+  messagingSenderId: "797167070547"
+};
+
+export const getData = () => {
+  throw new Error("Not Implemented");
+};
