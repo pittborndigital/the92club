bac3f4debb338e42df7308df0b9fc041722aef65
Rusta <rustauk@gmail.com>
2017-06-23 22:02:13 +0100

    added config with example values for colors and (controversial / optional) breakpoints

--- src/component/App/styled.js
+++ src/component/App/styled.js
@@ -1,9 +1,13 @@
 import styled from 'styled-components'
+import { colors, breakpoints } from '../../config'
 
 export const AppContainer = styled.div`
-  background-color: #ebecee;
+  background-color: ${colors.primary};
 `
 
 export const Title = styled.h1`
-  color: #274aa2;
+  color: ${colors.secondary};
+  @media (min-width: ${breakpoints.small}) {
+    color: green;
+  }
 `
---
+++ src/config/index.js
@@ -0,0 +1,8 @@
+export const colors = {
+  primary: 'red',
+  secondary: 'blue',
+}
+
+export const breakpoints = {
+  small: '450px',
+}
