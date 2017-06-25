e80b38421762b8a4e9a4e30312985d7fc8248700
Christopher Pitt <c.james.pitt@gmail.com>
2017-06-25 11:19:44 +0100

    add nightwatch tests for homepage and general routing

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # the92club
 The 92 club web app
+
+# e2e tests
+The e2e tests run using [NightwatchJS](http://nightwatchjs.org/), and Selenium Server. Selenium Server runs as a Java process, and therefore has some dependencies outside of this project.
+
+1. Install Java SDK (at least v7)
+  >http://www.oracle.com/technetwork/java/javase/downloads/index.html
---
+++ e2e-tests/general/routing.js
@@ -0,0 +1,16 @@
+module.exports = {
+  'Homepage loads': (client) => {
+    client
+      .url(client.launch_url)
+      .waitForElementVisible('body', 1000)
+      .assert.containsText('#root', 'Hello World')
+      .end()
+  },
+  'Catch bad routes with 404 page': (client) => {
+    client
+      .url(`${client.launch_url}/${(Math.random() * 1000).toString()}`)
+      .waitForElementVisible('body', 1000)
+      .assert.containsText('#root', 'Page not found')
+      .end()
+  },
+}
