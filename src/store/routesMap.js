4cbb6fe3e91d1958b31a5ddc4b7ec83561177773
Christopher Pitt <c.james.pitt@gmail.com>
2017-06-25 21:19:31 +0100

    additional component for more context

--- src/store/routesMap.js
+++ src/store/routesMap.js
@@ -1,9 +1,11 @@
 const NAV_PREFIX = 'NAVIGATE'
 
 export const ACTION_HOME = `${NAV_PREFIX}_HOME`
+export const ACTION_LEAGUE = `${NAV_PREFIX}_LEAGUE`
 export const ACTION_404 = `${NAV_PREFIX}_404`
 
 export default {
   [ACTION_HOME]: '/',
+  [ACTION_LEAGUE]: '/league/:leagueId',
   [ACTION_404]: '/404',
 }
