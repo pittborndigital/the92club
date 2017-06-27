3345949f651f88f9d72c178655dad746d7d8cf3e
Rusta <rustauk@gmail.com>
2017-06-27 18:35:06 +0100

    other missing gumpf to unflow the flow of this crazy river

---
+++ src/component/Achievement/styled.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+import { colors } from 'config'
+
+export const Container = styled.div`
+`
+Container.displayName = 'Achievement.Container'
+
+export const ProgressDisplay = styled.span`
+  color: ${colors.secondary};
+`
+ProgressDisplay.displayName = 'Achievement.ProgressDisplay'
